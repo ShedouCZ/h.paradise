4b9f9bb900fee83a5eff12b95b3680b9261845d0
Vojtěch Laňka <Wajnes@seznam.cz>
2015-09-15 19:21:43 +0200

    first commit

---
+++ Config/Schema/tables.sql
@@ -0,0 +1,22 @@
+-- Adminer 4.2.1 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8_czech_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `mail` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+INSERT INTO `users` (`id`, `username`, `password`, `mail`, `created`, `modified`) VALUES
+(1,	'admin',	'$2a$10$oTInOZNrfJqOdFEjVAb00ugqLRwquceqkt39aam1pY/4m1/py4.nq',	NULL,	'2015-06-24 10:37:25',	'2015-06-24 10:37:25');
+
+-- 2015-08-26 18:58:20
